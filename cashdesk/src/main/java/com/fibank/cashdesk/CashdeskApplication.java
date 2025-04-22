c9e0c58835ba29db7fa07c19054f31ee8372248a
Rado <rado88x@gmail.com>
2025-04-22 12:08:44 +0300

    Init commit some dependencies

---
+++ cashdesk/src/main/java/com/fibank/cashdesk/CashdeskApplication.java
@@ -0,0 +1,13 @@
+package com.fibank.cashdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashdeskApplication.class, args);
+	}
+
+}
